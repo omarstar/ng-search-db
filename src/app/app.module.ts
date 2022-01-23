4f8b7daa545e725c0add9b3780619cc0e07b8531
omar star <obarakat12@gmail.com>
2022-01-23 18:42:31 +0200

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,13 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { GaugeModule } from 'angular-gauge'
+import { MatTabsModule } from '@angular/material/tabs'
+import { MatSelectModule } from '@angular/material/select'
+import { MatIconModule } from '@angular/material/icon'
+import { MatFormFieldModule } from '@angular/material/form-field'
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +16,14 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    GaugeModule.forRoot(),
+    MatTabsModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
